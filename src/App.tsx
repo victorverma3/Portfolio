1b40bad550a3783a686ebf78f689e9e8da52919a
victor <victor.verma@icloud.com>
2023-05-27 13:48:51 -0400

    removed unncessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <Header />

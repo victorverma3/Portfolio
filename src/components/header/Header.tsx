22301d1f2e5580e05cae2b00374ae8361e470e28
victor <victor.verma@icloud.com>
2023-05-27 13:47:29 -0400

    initial commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Victor Verma</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Experiences</Nav.Link>
+              <Nav.Link href="#link">Academics</Nav.Link>
+              <Nav.Link href="#home">Projects</Nav.Link>
+              <Nav.Link href="#link">Resume</Nav.Link>
+              <Nav.Link href="#home">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;

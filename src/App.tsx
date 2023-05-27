ccc02ede76559417c6e44bc334b13126d86224a7
victor <victor.verma@icloud.com>
2023-05-27 14:11:24 -0400

    changed home bio message

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,13 +4,14 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="jumbotron">
-      <h1 className="display-4">Hello Everyone</h1>
-      <p className="lead">I am Victor Verma</p>
-      <hr className="my-4" />
-      <p>
-        It uses utility classes for typography and spacing to space content out
-        within the larger container.
+      <h1 className="display-4">Hello Everyone!</h1>
+      <p className="lead">
+        I am a third-year student at Boston University majoring in mathematics
+        and computer science, and minoring in data science. Explore my personal
+        website to learn about me!
       </p>
+      <hr className="my-4" />
+      <p>Explore my personal website to get to know about me!</p>
       <a className="btn btn-primary btn-lg" href="#" role="button">
         Learn more
       </a>

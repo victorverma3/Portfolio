3f7b37377b6b9af195b44e5be5c32922d8235a48
victor <victor.verma@icloud.com>
2023-05-27 15:32:59 -0400

    fixed bio grammar

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h1 className="display-4">Hello Everyone!</h1>
       <p className="lead">
         I am a third-year student at Boston University majoring in mathematics
-        and computer science, and minoring in data science. Explore my personal
+        and computer science and minoring in data science. Explore my personal
         website to learn about me!
       </p>
     </div>

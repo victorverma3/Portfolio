b2e8bd0ae5df6cc57e29efc7f4353bfa3a4b6e97
victor <victor.verma@icloud.com>
2023-05-28 18:13:16 -0400

    working on pathing image

--- src/components/mainCard/MainCard.tsx
+++ src/components/mainCard/MainCard.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./MainCard.css";
 import Card from "react-bootstrap/Card";
+import image from "../../assets/ikea.jpeg";
 
 const MainCard = () => {
   return (
     <div>
       <Card>
-        <Card.Img variant="top" src="" />
+        <Card.Img variant="top" src={image} alt="error loading image" />
         <Card.Body>
           <Card.Text>I like solving problems.</Card.Text>
         </Card.Body>

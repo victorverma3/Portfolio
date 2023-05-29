612bacc276f999a33d735469bd1a15299b26f5b0
victor <victor.verma@icloud.com>
2023-05-29 12:28:34 -0400

    horizontally stacked

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,16 +8,22 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div className="jumbotron">
-        <h1 className="display-4">Hello Everyone!</h1>
-        <p className="lead">
-          I am a third-year student at Boston University majoring in mathematics
-          and computer science and minoring in data science. Explore my personal
-          website to learn about me!
-        </p>
+      <div className="content">
+        <div className="jumbotron">
+          <h1 className="display-4">Hello Everyone!</h1>
+          <p className="lead">
+            I am a third-year student at Boston University majoring in
+            mathematics and computer science and minoring in data science.
+            Explore my personal website to learn about me!
+          </p>
+        </div>
+        <div className="pic">
+          <MainCard />
+        </div>
       </div>
-      <MainCard />
-      <Footer />
+      <div className="bottom">
+        <Footer />
+      </div>
     </>
   );
 };
--- src/components/mainCard/MainCard.tsx
+++ src/components/mainCard/MainCard.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./MainCard.css";
 import Card from "react-bootstrap/Card";
-import image from "../../images/vic.png";
+import image from "../../images/vic.jpg";
 
 const MainCard = () => {
   return (
     <div>
-      <Card>
+      <Card className="mainPic">
         <Card.Img variant="top" src={image} alt="error loading image" />
         <Card.Body>
           <Card.Text>I like solving problems.</Card.Text>

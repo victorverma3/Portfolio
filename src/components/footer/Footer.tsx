89a0143093a3a8eca4d6a6ee94308f5ee39e6380
victor <victor.verma@icloud.com>
2023-05-29 21:09:08 -0400

    working to center footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,30 +4,27 @@ import ListGroup from "react-bootstrap/ListGroup";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <ListGroup variant="horizontal">
-        <ListGroup.Item>
-          <a href="tel:1-617-838-4092">617-838-4092</a>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {" "}
-          <a href="mailto:vpverm@bu.edu">vpverm@bu.edu</a>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <a
-            href="https://www.linkedin.com/in/victor-verma-91713022b/"
-            target="_blank"
-          >
-            LinkedIn
-          </a>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <a href="https://github.com/victorverma3" target="_blank">
-            GitHub
-          </a>
-        </ListGroup.Item>
-      </ListGroup>
-    </div>
+    <ListGroup horizontal className="footer">
+      <ListGroup.Item>
+        <a href="tel:1-617-838-4092">617-838-4092</a>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <a href="mailto:vpverm@bu.edu">vpverm@bu.edu</a>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <a
+          href="https://www.linkedin.com/in/victor-verma-91713022b/"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <a href="https://github.com/victorverma3" target="_blank">
+          GitHub
+        </a>
+      </ListGroup.Item>
+    </ListGroup>
   );
 };
 

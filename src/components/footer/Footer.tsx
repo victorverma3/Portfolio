49befcd483e33c52848b208d8fb48adf1068061e
victor <victor.verma@icloud.com>
2023-06-04 20:28:53 -0400

    used custom footer instead of ListGroup

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,34 +5,41 @@ import ListGroup from "react-bootstrap/ListGroup";
 const Footer = () => {
   return (
     <div className="bottom">
-      <ListGroup horizontal className="footer">
-        <ListGroup.Item>
-          <a>617-838-4092</a>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <a className="footerLink" href="mailto:vpverm@bu.edu">
-            vpverm@bu.edu
-          </a>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <a
-            className="footerLink"
-            href="https://www.linkedin.com/in/victor-verma-91713022b/"
-            target="_blank"
-          >
-            LinkedIn
-          </a>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <a
-            className="footerLink"
-            href="https://github.com/victorverma3"
-            target="_blank"
-          >
-            GitHub
-          </a>
-        </ListGroup.Item>
-      </ListGroup>
+      <div className="footerItem">
+        <a>617-838-4092</a>
+      </div>
+      <div className="footerItem">
+        <a>|</a>
+      </div>
+      <div className="footerItem">
+        <a className="footerLink" href="mailto:vpverm@bu.edu">
+          vpverm@bu.edu
+        </a>
+      </div>
+      <div className="footerItem">
+        <a>|</a>
+      </div>
+      <div className="footerItem">
+        <a
+          className="footerLink"
+          href="https://www.linkedin.com/in/victor-verma-91713022b/"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+      </div>
+      <div className="footerItem">
+        <a>|</a>
+      </div>
+      <div className="footerItem">
+        <a
+          className="footerLink"
+          href="https://github.com/victorverma3"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
   );
 };

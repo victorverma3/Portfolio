53411191daf235da7af0fdc81c6f707b4e9db783
victor <victor.verma@icloud.com>
2023-06-10 19:11:58 -0400

    edited text

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
           mathematics and computer science and minoring in data science. I have
           no idea what I want to do in the future, so I just create whatever
           pops into my head. Sometimes these projects have a functional purpose
-          in my life; otherwise, I just work on what I am feeling passionate
+          in my life; otherwise, I just work on whatever I am feeling passionate
           about...
         </p>
       </div>

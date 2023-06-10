e2963e3c7f17b06511a645caa0bfd06ac4d86870
victor <victor.verma@icloud.com>
2023-06-10 17:49:27 -0400

    added bio

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
       hobby: "Gym",
       image: "placeholder",
       description:
-        "I enjoy going to the gym in order to maintain a healthy and active lifestyle. I rotate my split between Push Pull Legs (PPL) and Arnold.",
+        "I enjoy going to the gym in order to maintain a fun, healthy, and active lifestyle. I rotate my split between Push Pull Legs (PPL) and Arnold.",
       link: "placeholder",
       url: "placeholder",
     },
@@ -35,6 +35,24 @@ const About = () => {
     <>
       <div className="aboutContent">
         <h1 className="pageTitle">About Me</h1>
+        <p className="bio">
+          My name is Victor Verma, and I'm a 20-year old who was born and raised
+          in central Massachusetts. Throughout my whole life I've loved sports,
+          primarily football, basketball, and soccer. I played football for 10
+          years and was a starter at left-guard at the varsity level for my high
+          school. Additonally, I enjoy traveling the world with my family,
+          hanging out with my friends, and watching movies with whoever is
+          interested.
+        </p>
+        <p className="bio">
+          I have a passion for learning, and often watch youtube videos on
+          topics ranging from STEM fields such as computer science and physics,
+          to the social sciences and humanities as well. I really love to keep
+          up with ideas in international relations and political science, and I
+          find it equally fascinating to learn about history. Sometimes I even
+          tell my friends that I wish I could experience college as an infinite
+          loop, and pick a new major everytime.
+        </p>
         <h2 className="pageSubTitle">Hobbies</h2>
         <br></br>
         <CardList cards={aboutCards} />

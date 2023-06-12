9048f65b793a8e3a1cb3278af6d6a20c9c074f01
victor <victor.verma@icloud.com>
2023-06-11 20:01:07 -0400

    implemented resume download button

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Resume.css";
 import Footer from "../../components/footer/Footer";
+import resume from "../../images/resume.png";
+import PDFDownload from "../../components/pdfdownload/PDFDownload";
 
 const Resume = () => {
+  const resumePDF = {
+    source: "resume.pdf",
+  };
   return (
     <>
       <div className="resumeContent">
         <h1 className="pageTitle">Resume</h1>
+        <img src={resume} alt="image not loading"></img>
+      </div>
+      <div className="resumeDownload">
+        <PDFDownload pdf={resumePDF} />
       </div>
       <div className="bottom">
         <Footer />

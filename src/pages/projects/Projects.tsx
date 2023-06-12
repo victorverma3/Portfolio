cdade6e57094146fe5573c93bc78d816175fe40f
victor <victor.verma@icloud.com>
2023-06-11 23:26:10 -0400

    added 8-Puzzle Solver description

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
       title: "SportsNews",
       image: "placeholder",
       description:
-        "Daily email newsletter built using Python, HTML, and Bootstrap CSS that scrapes the latest sports headlines and sends them in email format to users on the mailing list. Deployed using Heroku in order to automate the script to run every morning at 9:00am est. Supported sports include basketball, college basketball, college football, cricket, football, and soccer.",
+        "Daily email newsletter built using Python, HTML, and Bootstrap CSS that scrapes the latest headlines in sports and sends them in email format to users on the mailing list. Deployed using Heroku in order to automate the script to run daily at 9:00am est. Currently supported sports include basketball, college basketball, college football, cricket, football, and soccer, with further sports coverage in development.",
       link: "Github",
       url: "https://github.com/victorverma3/SportsNews",
     },
@@ -24,7 +24,7 @@ const Projects = () => {
       title: "IGBot",
       image: "placeholder",
       description:
-        "Simple script that utilizes the user's inputted Instagram data to create a CSV that lists all of the accounts the user is following on Instagram that do not follow the user back in return. Many scripts for this task already exist on the internet, but I decided to create my own that I find easy to follow and can be used by my friends who are unfamilliar with programming.",
+        "Simple script that utilizes the user's inputted Instagram data to create a CSV that lists all of the accounts the user is following on Instagram that do not follow the user back in return. Many scripts for this task already exist on the internet, but I decided to create my own version that is customized to my needs and can be used easily by friends, especially those who are unfamilliar with programming.",
       link: "Github",
       url: "https://github.com/victorverma3/IGBot",
     },
@@ -32,7 +32,7 @@ const Projects = () => {
       title: "8-Puzzle Solver",
       image: "placeholder",
       description:
-        "I used Python to develop an 8-puzzle solver which found the shortest move combination to sort a 3x3 grid consisting of one blank space and the digits 1-8 in ascending order. Used object-oriented programming to create a board class, state class, and searcher class. Implemented BFS, DFS, Greedy Search, and a heuristic function to build the solver.",
+        "Used Python to develop an 8-puzzle solver which finds the shortest move combination that can be used to sort a 3x3 grid consisting of one blank space and the digits 1-8 in ascending order. The solver uses object-oriented programming to create a board, state, and searcher classes. BFS, DFS, Greedy Search, and heuristic functions are implemented and allow the solver to work efficiently.",
       link: "Github",
       url: "https://github.com/victorverma3/8-Puzzle-Solver",
     },

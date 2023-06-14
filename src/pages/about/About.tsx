1cd7c30fc3d670265fd83bca9dcb901dbc29d046
victor <victor.verma@icloud.com>
2023-06-14 17:23:04 -0400

    updating about me bio

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -44,18 +44,17 @@ const About = () => {
           in central Massachusetts. Throughout my whole life I've loved sports,
           primarily football, basketball, and soccer. I played football for 10
           years and was a starter at left-guard at the varsity level for my high
-          school. Additonally, I enjoy traveling the world with my family,
-          hanging out with my friends, and watching movies with whoever is
-          interested.
+          school team. Additionally, I enjoy hanging out with my friends, and
+          watching movies with whoever is interested.
         </p>
         <p className="aboutBio">
           I have a passion for learning, and often watch youtube videos on
-          topics ranging from STEM fields such as computer science and physics,
-          to the social sciences and humanities as well. I really love to keep
-          up with ideas in international relations and political science, and I
-          find it equally fascinating to learn about history. Sometimes I even
-          tell my friends that I wish I could experience college as an infinite
-          loop, and pick a new major everytime.
+          topics ranging from computer science and physics, to the social
+          sciences and humanities as well. I especially love to keep up with
+          ideas in the fields of international relations and political science,
+          and I find it equally fascinating to learn about history. Sometimes I
+          even tell my friends that I wish I could experience college as an
+          infinite loop, and pick a new major everytime.
         </p>
         <h2 className="pageSubTitle">Hobbies</h2>
         <CardList cards={aboutCards} />

4e635fe2d629f5efba36dce5083c6bc5d18feb0f
victor <victor.verma@icloud.com>
2023-06-20 00:04:28 -0400

    updated experience router link

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/experiences",
+        path: "/experience",
         element: <Experience />,
       },
       {

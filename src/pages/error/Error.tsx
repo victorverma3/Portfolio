5167f2750bc6fc15b675b21d8b35fe317d642776
victor <victor.verma@icloud.com>
2023-06-20 10:02:43 -0400

    resized error image and added footer

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Error.css";
 import image from "../../images/tom.jpg";
+import Footer from "../../components/Footer/Footer";
 
 const Error = () => {
   return (
-    <img className="errorImage" src={image} alt="error loading image"></img>
+    <>
+      <img className="errorImage" src={image} alt="error loading image"></img>
+      <Footer />
+    </>
   );
 };
 

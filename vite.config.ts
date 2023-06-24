94c38f8dfa48b314b2582192bc5268de6abe2ffc
victor <victor.verma@icloud.com>
2023-06-24 11:50:46 -0400

    trying deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  esbuild: {
+    loader: 'tsx',
+}, 
+root: './',
+build: {
+    outDir: './build',
+},
+optimizeDeps: {
+    esbuildOptions: {
+        loader: {
+            '.js': 'jsx',
+            '.ts': 'tsx',
+        },
+    },
+},
+plugins: [react()],
 })

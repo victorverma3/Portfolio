39adcddcb72f765a0b83d895785c3ed2bc8d163d
victor <victor.verma@icloud.com>
2023-06-24 19:06:49 -0400

    testing footer import

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-import Footer from "../../components/Footer/Footer.tsx";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (

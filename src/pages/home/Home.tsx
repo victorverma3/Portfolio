771814893bc75b178a0c21c8b55c45a1b77bd0f4
victor <victor.verma@icloud.com>
2023-06-25 19:18:36 -0400

    updated home lead

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,12 +6,12 @@ const Home = () => {
   return (
     <>
       <div className="homeContent">
-        <h1 className="homeTitle">Hello Everyone!</h1>
+        <h1 className="homeTitle">Hi, I'm Victor.</h1>
         <p className="homeLead">
-          I'm Victor, a third-year student at Boston University majoring in
-          mathematics and computer science and minoring in data science. I have
-          no idea what I want to do in the future, so I try to learn a little
-          bit of everything and create whatever I find interesting...
+          I'm a third-year student at Boston University majoring in mathematics
+          and computer science and minoring in data science. I have no idea what
+          I want to do in the future, so I learn and create whatever I find
+          interesting...
         </p>
       </div>
       <div className="bottom">

d883cd2b6583e9f9022ea3ed3de578a4402e2938
victor <victor.verma@icloud.com>
2023-06-25 19:58:45 -0400

    testing resume button

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -3,10 +3,11 @@ import "./Resume.css";
 import Footer from "../../components/footer/Footer";
 import resume from "../../images/resume.png";
 import PDFView from "../../components/pdfview/PDFView";
+import { Button } from "react-bootstrap";
 
 const Resume = () => {
-  const resumePDF = {
-    source: "resume.pdf",
+  const onButtonClick = () => {
+    window.open("../../../public/VictorVerma.pdf", "_blank");
   };
   return (
     <>
@@ -14,7 +15,9 @@ const Resume = () => {
         <h1 className="pageTitle">Resume</h1>
         <img className="resumePic" src={resume} alt="image not loading"></img>
         <div className="resumeView">
-          <PDFView pdf={resumePDF} />
+          <Button className="pdfButton" variant="dark" onClick={onButtonClick}>
+            View as PDF
+          </Button>
         </div>
       </div>
       <div className="bottom">

25fe74ee4a6bdc300a749ac95294a5f232025ff9
victor <victor.verma@icloud.com>
2023-06-26 18:56:36 -0400

    Github to GitHub

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -17,7 +17,7 @@ const Projects = () => {
       image: website,
       description:
         "Designed and created my personal portfolio website using TypeScript, HTML, and Bootstrap CSS in a Vite + React framework. Primarily used YouTube and Google to independently learn TypeScript and React and expand my previous knowledge of HTML and CSS. Deployed using Vercel in order to make the website accessible to the internet, and periodically receives maintenance and improvements. ",
-      link: "Github",
+      link: "GitHub",
       url: "https://github.com/victorverma3/Portfolio",
     },
     {
@@ -25,7 +25,7 @@ const Projects = () => {
       image: sports,
       description:
         "Daily email newsletter built using Python, HTML, and CSS that scrapes the latest headlines in sports and sends them via an email to users on the mailing list. Deployed using Heroku in order to automate the script to run daily at 9:00am est. Currently supports basketball, college basketball, college football, cricket, football, and soccer, with plans to implement baseball, hockey, and more in the future.",
-      link: "Github",
+      link: "GitHub",
       url: "https://github.com/victorverma3/SportsNews",
     },
     {
@@ -33,7 +33,7 @@ const Projects = () => {
       image: reel,
       description:
         "Created software that utilizes the pandas and BeautifulSoup libraries in Python to efficiently compile the movie metadata for all movies that a specified user has rated on the Letterboxd app. The movie's title, user rating, Letterboxd rating, Letterboxd rating count, genres, and country of origin are output into a CSV file, which is then read to create summary histograms and a CSV file with summary statistics.",
-      link: "Github",
+      link: "GitHub",
       url: "https://github.com/victorverma3/MovieData",
     },
     {
@@ -41,7 +41,7 @@ const Projects = () => {
       image: bot,
       description:
         "Simple script that parses through the user's inputted JSON Instagram data and creates a CSV that lists the user's \"fakes\" (accounts that the user follows, but don't follow the user back) and \"fans\" (accounts that follow the user, but the user doesn't follow back. A future goal is for the script to gather the user's following and follower data with just the username as input (though only with the consent of the user).  ",
-      link: "Github",
+      link: "GitHub",
       url: "https://github.com/victorverma3/IGBot",
     },
     {
@@ -49,7 +49,7 @@ const Projects = () => {
       image: puzzle,
       description:
         "Used Python to develop an 8-puzzle solver which finds the shortest move combination that can be used to sort a 3x3 grid consisting of one blank space and the digits 1-8 in ascending order. The solver uses object-oriented programming to create a board, state, and searcher classes. BFS, DFS, Greedy Search, and heuristic functions are implemented and allow the solver to work efficiently. Created in CS111.",
-      link: "Github",
+      link: "GitHub",
       url: "https://github.com/victorverma3/8-Puzzle-Solver",
     },
     {
@@ -57,7 +57,7 @@ const Projects = () => {
       image: connect4,
       description:
         'Used Python to create the game "Connect 4". Users can play against each other or an AI capable of predicting moves using recursive backtracking. The moves are given by user inputs and the game ends in a win, loss, or tie. The difficulty of the AI is defined by how many moves it can "look ahead". The board, player, and AI classes are made using object-oriented programming. Created in CS111.',
-      link: "Github",
+      link: "GitHub",
       url: "https://github.com/victorverma3/Connect4",
     },
   ];

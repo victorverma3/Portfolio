918668d85c915c5699d5f641057b6b35ac7a9590
victor <victor.verma@icloud.com>
2023-07-05 19:33:45 -0400

    rearranged order of text on eduCards

--- src/pages/education/Education.tsx
+++ src/pages/education/Education.tsx
@@ -21,20 +21,14 @@ const Education = () => {
                 </Card.Title>
                 <Card.Title>Minor in Data Science</Card.Title>
                 <Card.Text>September 2021 - May 2025</Card.Text>
-                <Card.Subtitle>Relevant Courses</Card.Subtitle>
+                <Card.Subtitle>Accomplishments</Card.Subtitle>
                 <Card.Text>
                   <ul>
+                    <li>3.83 GPA (3.83 major, 4.0 minor).</li>
                     <li>
-                      Applied Abstract Algebra, Differential Equations, Linear
-                      Algebra, Multivariate Calculus, and Probability.
+                      College of Arts and Sciences Dean's List (Spring 2023,
+                      Fall 2022, Spring 2022, Fall 2021).
                     </li>
-                    <li>
-                      Combinatoric Structures, Computer Systems, Concepts of
-                      Programming Languages, Introduction to Analysis of
-                      Algorithms, Introduction to Computer Science I & II,
-                      Probability in Computing, and Software Engineering.
-                    </li>
-                    <li>Introduction to Machine Learning and AI.</li>
                   </ul>
                 </Card.Text>
                 <Card.Subtitle>Extracurriculars</Card.Subtitle>
@@ -52,14 +46,20 @@ const Education = () => {
                     <li>Member of the Boston University Barbell Club.</li>
                   </ul>
                 </Card.Text>
-                <Card.Subtitle>Accomplishments</Card.Subtitle>
+                <Card.Subtitle>Relevant Courses</Card.Subtitle>
                 <Card.Text>
                   <ul>
-                    <li>3.83 GPA (3.83 major, 4.0 minor).</li>
                     <li>
-                      College of Arts and Sciences Dean's List (Spring 2023,
-                      Fall 2022, Spring 2022, Fall 2021).
+                      Applied Abstract Algebra, Differential Equations, Linear
+                      Algebra, Multivariate Calculus, and Probability.
                     </li>
+                    <li>
+                      Combinatoric Structures, Computer Systems, Concepts of
+                      Programming Languages, Introduction to Analysis of
+                      Algorithms, Introduction to Computer Science I & II,
+                      Probability in Computing, and Software Engineering.
+                    </li>
+                    <li>Introduction to Machine Learning and AI.</li>
                   </ul>
                 </Card.Text>
               </Card.Body>
@@ -85,24 +85,6 @@ const Education = () => {
               <Card.Body>
                 <Card.Title>High School Diploma</Card.Title>
                 <Card.Text>September 2017 - June 2021</Card.Text>
-                <Card.Subtitle>Relevant Courses</Card.Subtitle>
-                <Card.Text>
-                  <ul>
-                    <li>
-                      AP Calculus BC, AP Statistics, and Honors Mathematical
-                      Modeling.
-                    </li>
-                  </ul>
-                </Card.Text>
-                <Card.Subtitle>Extracurriculars</Card.Subtitle>
-                <Card.Text>
-                  <ul>
-                    <li>Varsity Football Starter.</li>
-                    <li>Member of the Société Honoraire de Français.</li>
-                    <li>Lab Assistant for Honors Chemistry.</li>
-                    <li>Junior Varsity Track and Field.</li>
-                  </ul>
-                </Card.Text>
                 <Card.Subtitle>Accomplishments</Card.Subtitle>
                 <Card.Text>
                   <ul>
@@ -117,6 +99,24 @@ const Education = () => {
                     <li>
                       Junior Varsity Football Player of the Week (2x) (voted by
                       the coaches).
+                    </li>
+                  </ul>
+                </Card.Text>
+                <Card.Subtitle>Extracurriculars</Card.Subtitle>
+                <Card.Text>
+                  <ul>
+                    <li>Varsity Football Starter.</li>
+                    <li>Member of the Société Honoraire de Français.</li>
+                    <li>Lab Assistant for Honors Chemistry.</li>
+                    <li>Junior Varsity Track and Field.</li>
+                  </ul>
+                </Card.Text>
+                <Card.Subtitle>Relevant Courses</Card.Subtitle>
+                <Card.Text>
+                  <ul>
+                    <li>
+                      AP Calculus BC, AP Statistics, and Honors Mathematical
+                      Modeling.
                     </li>
                   </ul>
                 </Card.Text>

b6ab17948e86d8a19772ce4a5199594dcca75472
victor <victor.verma@icloud.com>
2023-07-05 19:37:37 -0400

    removed high school education

--- src/pages/education/Education.tsx
+++ src/pages/education/Education.tsx
@@ -78,55 +78,6 @@ const Education = () => {
             ></img>
           </div>
         </div>
-        <div className="school">
-          <div className="schoolInfo">
-            <Card className="eduCard">
-              <Card.Header as="h5">Shrewsbury High School</Card.Header>
-              <Card.Body>
-                <Card.Title>High School Diploma</Card.Title>
-                <Card.Text>September 2017 - June 2021</Card.Text>
-                <Card.Subtitle>Accomplishments</Card.Subtitle>
-                <Card.Text>
-                  <ul>
-                    <li>4.938 GPA and AP Scholar with Distinction.</li>
-                    <li>
-                      1 of 8 recipients of the Town of Shrewsbury Scholarship.
-                    </li>
-                    <li>
-                      Varsity Football Lineman of the Week (voted by the
-                      coaches).
-                    </li>
-                    <li>
-                      Junior Varsity Football Player of the Week (2x) (voted by
-                      the coaches).
-                    </li>
-                  </ul>
-                </Card.Text>
-                <Card.Subtitle>Extracurriculars</Card.Subtitle>
-                <Card.Text>
-                  <ul>
-                    <li>Varsity Football Starter.</li>
-                    <li>Member of the Société Honoraire de Français.</li>
-                    <li>Lab Assistant for Honors Chemistry.</li>
-                    <li>Junior Varsity Track and Field.</li>
-                  </ul>
-                </Card.Text>
-                <Card.Subtitle>Relevant Courses</Card.Subtitle>
-                <Card.Text>
-                  <ul>
-                    <li>
-                      AP Calculus BC, AP Statistics, and Honors Mathematical
-                      Modeling.
-                    </li>
-                  </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="schoolPicContainer">
-            <img className="schoolPic" src={shs} alt="image not loading"></img>
-          </div>
-        </div>
       </div>
       <div className="bottom">
         <Footer />

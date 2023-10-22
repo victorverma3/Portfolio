c1bd41cbdbf4ee1cc45f28dc669cabb85ae4a333
victor <victor.verma@icloud.com>
2023-10-21 21:03:45 -0400

    fixing cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'https://victor-verma-portfolio-frontend.vercel.app',
+        methods: 'GET',
+    }
+));
 
 app.get('/', (request, response) => {
     console.log(request)

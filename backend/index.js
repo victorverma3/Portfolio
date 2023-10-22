0bff660d6c7c2803c98a05a4ad0b0ed66b3277e7
victor <victor.verma@icloud.com>
2023-10-22 09:47:20 -0400

    updating syntax

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-import {PORT, mongoDBURL} from "./config.js";
-import experienceRoute from './routes/experienceRoute.js';
-import mongoose, { model } from 'mongoose';
+import express from "express";
+import cors from "cors";
+import { PORT, mongoDBURL } from "./config.js";
+import experienceRoute from "./routes/experienceRoute.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-    origin: 'https://victor-verma-portfolio.vercel.app'
-}));
+app.use(
+  cors({
+    origin: "https://victor-verma-portfolio.vercel.app",
+  })
+);
 
-app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(234).send(`Backend for Victor's Portfolio`)
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send(`Backend for Victor's Portfolio`);
 });
 
-app.use('/experience-collection', experienceRoute);
+app.use("/experience-collection", experienceRoute);
 
 mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-        console.log('App connected to database');
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`)
-        });
-    })
-    .catch((error) => {
-        console.log(error);
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 export default app;

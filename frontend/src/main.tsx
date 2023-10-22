cdae7e2b10ac8256cceb82ba3114911865ff2027
victor <victor.verma@icloud.com>
2023-10-22 10:32:10 -0400

    removed analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { inject } from "@vercel/analytics";
-
-inject();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

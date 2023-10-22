64d6dc113090e56274805f35a5124877afe99738
victor <victor.verma@icloud.com>
2023-10-22 18:11:08 -0400

    updated origin for cors to allow localhost

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://victor-verma-portfolio.vercel.app",
+    origin: ["https://victor-verma-portfolio.vercel.app", "http://localhost"],
   })
 );
 

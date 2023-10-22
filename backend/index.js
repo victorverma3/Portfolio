de87071444939786f45a586f28a148ccfdb6cb16
victor <victor.verma@icloud.com>
2023-10-21 21:13:33 -0400

    trying cors solution

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,30 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors(
+/* app.use(cors(
     {
-        origin: 'https://victor-verma-portfolio-frontend.vercel.app',
+        origin: 'https://victor-verma-portfolio-frontend.vercel.app/*',
         methods: 'GET',
     }
-));
+)); */
+
+app.use((req, res, next) => {
+    const allowedOrigins = [
+        'https://victor-verma-portfolio-frontend.vercel.app', // Your frontend URL
+    ];
+
+    const origin = req.get('origin');
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
 
 app.get('/', (request, response) => {
     console.log(request)

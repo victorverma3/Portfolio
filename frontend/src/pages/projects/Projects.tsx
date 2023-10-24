1021d3fe9cfe66cdc67eb55fd984541f27c05d84
victor <victor.verma@icloud.com>
2023-10-23 23:22:39 -0400

    updated project card description and sizing

--- frontend/src/pages/projects/Projects.tsx
+++ frontend/src/pages/projects/Projects.tsx
@@ -32,6 +32,35 @@ const Projects = () => {
         console.log(error);
       });
   }, []);
+  const test = [
+    {
+      title: "CandidateBios",
+      image: "test",
+      description:
+        "I designed Python software to gather the biodata of 150,000 U.S. state legislators from the web. I utilized the Google Search API, ChatGPT API, BeautifulSoup, and pandas. I also implemented multithreading, rate-limiting, and error-handling.",
+      technologies: ["test"],
+      links: [["test"]],
+      sortOrder: 1,
+    },
+    {
+      title: "Portfolio Website",
+      image: "test",
+      description:
+        "I created this portfolio website with the MERN stack using TypeScript and Bootstrap. I deployed both the backend and frontend on Vercel. The site periodically receives maintenance and improvements as I accomplish more and learn new skills.",
+      technologies: ["test"],
+      links: [["Live Demo"], ["GitHub"]],
+      sortOrder: 2,
+    },
+    {
+      title: "SportsNews",
+      image: "test",
+      description:
+        "I developed a Python script to facilitate the real-time news retrieval for 6 sports, automating headline formatting, data scraping, and newsletter creation. I used HTML and CSS to design email newsletters that are delivered daily at 9:00am EST.",
+      technologies: ["test"],
+      links: [["GitHub"]],
+      sortOrder: 2,
+    },
+  ];
   return (
     <div className="project-content">
       <h1 className="page-title">Projects</h1>

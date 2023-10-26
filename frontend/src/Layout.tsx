51f5fe9b101030929dd3e5600362b003beb2572d
victor <victor.verma@icloud.com>
2023-10-25 21:59:57 -0400

    trying import

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
+import Footer from "Footer";
+import Header from "Header";
 
 function Layout() {
   return (

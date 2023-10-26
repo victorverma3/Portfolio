0303e5a96c707b6b969cd9f7892b97ca2e27e678
victor <victor.verma@icloud.com>
2023-10-25 23:24:32 -0400

    added backend update experience route

--- backend/routes/experienceRoute.js
+++ backend/routes/experienceRoute.js
@@ -30,4 +30,34 @@ router.get("/:id", async (request, response) => {
   }
 });
 
+router.put("/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.role ||
+      !request.body.employer ||
+      !request.body.dates ||
+      !request.body.location ||
+      !request.body.description ||
+      !request.body.icon ||
+      !request.body.sortOrder
+    ) {
+      return response.status(400).send({
+        message:
+          "Send all required fields: Role, Employer, Dates, Location, Description, Icon, and Sort Order",
+      });
+    }
+    const { id } = request.params;
+    const result = await Experiences.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "Experience not found" });
+    }
+    return response
+      .status(200)
+      .send({ message: "Experience updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 export default router;

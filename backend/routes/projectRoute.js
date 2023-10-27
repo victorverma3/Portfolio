efa36541093f3294183194bc029e87571356e58a
victor <victor.verma@icloud.com>
2023-10-27 09:41:00 -0400

    added backend support for project editing

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -4,6 +4,7 @@ import { Projects } from "../models/projectModel.js";
 
 const router = express.Router();
 
+// get all projects
 router.get("/", async (request, response) => {
   try {
     const projectElements = await Projects.find({}).sort({
@@ -13,6 +14,48 @@ router.get("/", async (request, response) => {
       count: projectElements.length,
       data: projectElements,
     });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// get a project by id
+router.get("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const project = await Projects.findById(id);
+    return response.status(200).json(project);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// update a project
+router.put("/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.image ||
+      !request.body.description ||
+      !request.body.technologies ||
+      !request.body.links ||
+      !request.body.sortOrder
+    ) {
+      return response.status(400).send({
+        message:
+          "Send all required fields: Title, Image, Description, Technologies, Links, and Sort Order",
+      });
+    }
+    const { id } = request.params;
+    const result = await Projects.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "Project not found" });
+    }
+    return response
+      .status(200)
+      .send({ message: "Project updated successfully" });
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });

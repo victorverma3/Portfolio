5eeb5efa54a480fdb8af3b5683257240e847e310
victor <victor.verma@icloud.com>
2023-10-27 09:47:51 -0400

    updating backend

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -19,4 +19,46 @@ router.get("/", async (request, response) => {
   }
 });
 
+// get a project by id
+router.get("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const project = await Projects.findById(id);
+    return response.status(200).json(project);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// update a project
+router.put("/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.image ||
+      !request.body.description ||
+      !request.body.technologies ||
+      !request.body.links ||
+      !request.body.sortOrder
+    ) {
+      return response.status(400).send({
+        message:
+          "Send all required fields: Title, Image, Description, Technologies, Links, and Sort Order",
+      });
+    }
+    const { id } = request.params;
+    const result = await Projects.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "Project not found" });
+    }
+    return response
+      .status(200)
+      .send({ message: "Project updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 export default router;

1826ee5582c5165e5af4e9896af139e570ad8ead
victor <victor.verma@icloud.com>
2023-11-10 11:00:58 -0500

    working on education page

--- frontend/src/pages/education/Education.tsx
+++ frontend/src/pages/education/Education.tsx
@@ -1,81 +1,84 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+import PDF from "../../components/VictorVerma.pdf";
 
 import "./Education.css";
 
 import bueast from "../../images/bueast.png";
+import bulatinlogo from "../../images/bulatinlogo.png";
 import comlawn from "../../images/comlawn.png";
 
 const Education = () => {
   return (
     <div className="education-content">
       <h1 className="page-title">Education</h1>
-      <div className="school">
-        <div className="school-info">
-          <Card className="edu-card">
-            <Card.Header as="h5">Boston University</Card.Header>
-            <Card.Body>
-              <Card.Title>
-                Bachelor of Arts in Mathematics and Computer Science
-              </Card.Title>
-              <Card.Title>Minor in Data Science</Card.Title>
-              <Card.Text>September 2021 - May 2025</Card.Text>
-              <Card.Subtitle>Accomplishments</Card.Subtitle>
-              <Card.Text>
-                <ul>
-                  <li>3.83 GPA (3.83 major, 4.0 minor).</li>
-                  <li>
-                    College of Arts and Sciences Dean's List (Spring 2023, Fall
-                    2022, Spring 2022, Fall 2021).
-                  </li>
-                </ul>
-              </Card.Text>
-              <Card.Subtitle>Extracurriculars</Card.Subtitle>
-              <Card.Text>
-                <ul>
-                  <li>
-                    Undergraduate Research Assistant at the Questrom School of
-                    Business.
-                  </li>
-                  <li>Computer Assistant at the Questrom Open Access Lab.</li>
-                  <li>
-                    App Committee Head in the Kappa Theta Pi Professional
-                    Fraternity.
-                  </li>
-                  <li>Member of the Boston University Barbell Club.</li>
-                </ul>
-              </Card.Text>
-              <Card.Subtitle>Relevant Courses</Card.Subtitle>
-              <Card.Text>
-                <ul>
-                  <li>
-                    Applied Abstract Algebra, Differential Equations, Linear
-                    Algebra, Multivariate Calculus, and Probability.
-                  </li>
-                  <li>
-                    Combinatoric Structures, Computer Systems, Concepts of
-                    Programming Languages, Introduction to Analysis of
-                    Algorithms, Introduction to Computer Science I & II,
-                    Probability in Computing, and Software Engineering.
-                  </li>
-                  <li>Introduction to Machine Learning and AI.</li>
-                </ul>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="school-pic-container">
+      <div className="education-display">
+        <div className="education-display-header"></div>
+        <h3>Boston University</h3>
+        <h5>Expected Graduation - May 2025</h5>
+        <h5>B.A. in Mathematics and Computer Science</h5>
+        <h5>Minor in Data Science</h5>
+        <div className="education-display-body">
+          <div className="education-display-column">
+            <h5 className="education-display-subtitle">Accomplishments</h5>
+            <ul>
+              <li>3.83 GPA (3.83 major, 4.0 minor).</li>
+              <li>
+                College of Arts and Sciences Dean's List (Spring 2023, Fall
+                2022, Spring 2022, Fall 2021).
+              </li>
+            </ul>
+            <h5 className="education-display-subtitle">Extracurriculars</h5>
+            <ul>
+              <li>
+                Undergraduate Research Assistant at the Questrom School of
+                Business.
+              </li>
+              <li>Computer Assistant at the Questrom Open Access Lab.</li>
+              <li>
+                App Committee Head in the Kappa Theta Pi Professional
+                Fraternity.
+              </li>
+              <li>Member of the Boston University Barbell Club.</li>
+            </ul>
+          </div>
           <img
-            className="school-pic"
-            src={comlawn}
+            className="education-display-image"
+            src={bulatinlogo}
             alt="image not loading"
           ></img>
-          <img
-            className="school-pic"
-            src={bueast}
-            alt="image not loading"
-          ></img>
+          <div className="education-display-column">
+            <h5 className="education-display-subtitle">Relevant Courses</h5>
+            <ul>
+              <li>
+                Applied Abstract Algebra, Differential Equations, Linear
+                Algebra, Multivariate Calculus, and Probability.
+              </li>
+              <li>
+                Combinatoric Structures, Computer Systems, Concepts of
+                Programming Languages, Introduction to Analysis of Algorithms,
+                Introduction to Computer Science I & II, Probability in
+                Computing, and Software Engineering.
+              </li>
+              <li>Introduction to Machine Learning and AI.</li>
+            </ul>
+          </div>
         </div>
+      </div>
+      <div className="resume-view">
+        <Button
+          className="pdf-button"
+          variant="dark"
+          href={PDF}
+          target="_blank"
+        >
+          Resume
+        </Button>
+      </div>
+      <div className="school-pic-container">
+        <img className="school-pic" src={comlawn} alt="image not loading"></img>
+        <img className="school-pic" src={bueast} alt="image not loading"></img>
       </div>
     </div>
   );

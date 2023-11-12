b351c474e164f6816af1a3b3d0700a7c5fb698c7
victor <victor.verma@icloud.com>
2023-11-11 22:43:39 -0500

    working on redesign

--- frontend/src/pages/education/Education.tsx
+++ frontend/src/pages/education/Education.tsx
@@ -14,41 +14,28 @@ const Education = () => {
     <div className="education-content">
       <h1 className="page-title">Education</h1>
       <div className="education-display">
-        <div className="education-display-header"></div>
-        <h3>Boston University</h3>
-        <h5>Expected Graduation - May 2025</h5>
-        <h5>B.A. in Mathematics and Computer Science</h5>
-        <h5>Minor in Data Science</h5>
-        <div className="education-display-body">
-          <div className="education-display-column">
-            <h5 className="education-display-subtitle">Accomplishments</h5>
+        <div className="education-display-column">
+          <div className="education-display-item">
+            <img
+              className="education-display-image"
+              src={bulatinlogo}
+              alt="image not loading"
+            ></img>
+          </div>
+        </div>
+        <div className="education-display-column">
+          <div className="education-display-item">
+            <h5 className="education-display-subtitle">Boston University</h5>
             <ul>
+              <li>Expected Graduation - May 2025</li>
+              <li>B.A. in Mathematics and Computer Science</li>
+              <li>Minor in Data Science</li>
               <li>3.83 GPA (3.83 major, 4.0 minor).</li>
-              <li>
-                College of Arts and Sciences Dean's List (Spring 2023, Fall
-                2022, Spring 2022, Fall 2021).
-              </li>
-            </ul>
-            <h5 className="education-display-subtitle">Extracurriculars</h5>
-            <ul>
-              <li>
-                Undergraduate Research Assistant at the Questrom School of
-                Business.
-              </li>
-              <li>Computer Assistant at the Questrom Open Access Lab.</li>
-              <li>
-                App Committee Head in the Kappa Theta Pi Professional
-                Fraternity.
-              </li>
-              <li>Member of the Boston University Barbell Club.</li>
+              <li>$7500 Student Research Award (Summer 2023 - Fall 2023)</li>
+              <li>College of Arts and Sciences Dean's List.</li>
             </ul>
           </div>
-          <img
-            className="education-display-image"
-            src={bulatinlogo}
-            alt="image not loading"
-          ></img>
-          <div className="education-display-column">
+          <div className="education-display-item">
             <h5 className="education-display-subtitle">Relevant Courses</h5>
             <ul>
               <li>
@@ -62,6 +49,21 @@ const Education = () => {
                 Computing, and Software Engineering.
               </li>
               <li>Introduction to Machine Learning and AI.</li>
+            </ul>
+          </div>
+          <div className="education-display-item">
+            <h5 className="education-display-subtitle">Extracurriculars</h5>
+            <ul>
+              <li>
+                Undergraduate Research Assistant at the Questrom School of
+                Business.
+              </li>
+              <li>Computer Assistant at the Questrom Open Access Lab.</li>
+              <li>
+                App Committee Head in the Kappa Theta Pi Professional
+                Fraternity.
+              </li>
+              <li>Boston University Barbell Club.</li>
             </ul>
           </div>
         </div>

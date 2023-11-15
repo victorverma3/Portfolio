581f411b08edea23f04299336c2917cd2242715a
victor <victor.verma@icloud.com>
2023-11-15 10:12:54 -0500

    fixed css path typo

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./footer.css";
+import "./Footer.css";
 
 import githublogo from "../../images/githublogo.png";
 import maillogo from "../../images/maillogo.png";

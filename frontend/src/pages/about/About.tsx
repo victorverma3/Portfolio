e9633f0c4a7e1aa191dc3fb277b9faa23f3891e9
victor <victor.verma@icloud.com>
2023-11-17 10:35:53 -0500

    updated font family and added transition to edu item box shadow

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -46,35 +46,34 @@ const About = () => {
         <img className="about-pic" src={husky} alt="image loading error"></img>
       </div>
       <p className="about-bio">
-        My name is Victor Verma, and I'm a 20-year old who was born and raised
-        in central Massachusetts. I live with my parents, my younger sister, and
-        our dog. Throughout my whole life I've loved sports, primarily football,
-        basketball, and soccer. I played football for 10 years and started at
-        left-guard at the varsity level for my high school team. I'm extremely
-        proud of receiving multiple player of the week awards in high school and
-        being named as a Central Mass All-Star in 7th grade, but my accolades
-        pale in comparison to the close friends I made and the life lessons that
-        I learned while playing the sport.
+        I was born and raised in central Massachusetts, and live with my
+        parents, my younger sister, and our dog. I played football for 10 years
+        and started at left-guard at the varsity level for my high school team.
+        I'm extremely proud of receiving multiple individual awards in middle
+        school and high school, including being named a Central Mass All Star in
+        7th grade, but I will forever cherish the close friends and memories I
+        made and the life lessons that I learned while playing the sport.
       </p>
       <p className="about-bio">
-        I've been lucky to have had the opportunity to travel to many places
-        with my family, some of my favorites being Egypt, Iceland, and Alaska,
-        and I'm excited to continue traveling as I grow up. I have so many more
-        places I want to go, especially the African savanna and the Amazon
-        rainforest. I like nature and the outdoors, and I also want to go
-        camping in the wilderness.
+        My parents love to travel, and as a result, I have had the lucky
+        opportunity to go to many places growing up. Some of my favorite
+        locations include Egypt for its history, and Iceland and Alaska for
+        their scenery. I have so many more places I want to go, especially the
+        African savanna and the Amazon rainforest. I like nature and the
+        outdoors, and I also want to go camping in the wilderness.
       </p>
       <p className="about-bio">
-        I have a passion for learning. Even though a lot of my time is used up
-        with school, working, the gym, and other hobbies, I always spend a few
-        hours each week working on my own passion projects or learning new
-        things. I like to watch youtube videos on topics ranging from computer
-        science, mathematics, and physics, to the social sciences and humanities
-        as well. I especially love to immerse myself in the fields of
-        international relations, political science, and history. I actually tell
-        everyone that I wish I could experience college as an infinite loop and
-        pick a new major in every pass.
+        I consider myself to be a perpetual learner. Even though a lot of my
+        time is used up with school, working, the gym, and other hobbies, I
+        always spend a few hours each week working on my own passion projects or
+        learning new things. I like to watch youtube videos on topics ranging
+        from computer science, mathematics, and physics, to the social sciences
+        and humanities as well. I especially love to immerse myself in the
+        fields of international relations, geopolitical science, and history. I
+        tell everyone that I wish I could experience college as an infinite loop
+        and pick a new major in every pass.
       </p>
+
       <br></br>
       {loading ? <Spinner /> : <AboutCards cards={aboutData} />}
       <Disclaimer />

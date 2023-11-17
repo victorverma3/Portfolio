8c3810f1ea367cd9fb1ee3a467ed1c5728ca7710
victor <victor.verma@icloud.com>
2023-11-17 14:27:19 -0500

    rotating title text on home page

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,19 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 import Footer from "../../components/Footer/Footer";
 
 import "./Home.css";
 
 const Home = () => {
+  const titlePhrases = [
+    "Hi, I'm Victor.",
+    "Software Engineer",
+    "Academic Weapon",
+  ];
+  const [index, setIndex] = useState(0);
+  const [currentTitle, setCurrentTitle] = useState(titlePhrases[index]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % titlePhrases.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    setCurrentTitle(titlePhrases[index]);
+  }, [index]);
+
   return (
     <>
       <div className="home-content">
-        <h1 className="home-title">Hi, I'm Victor.</h1>
+        <h1 className="home-title">{currentTitle}</h1>
         <p className="home-lead">
           I'm a third-year student at Boston University majoring in mathematics
-          and computer science and minoring in data science. I have no idea what
-          I want to do in the future, so I spend my time learning what I find
-          interesting and creating projects that enhance my life.
+          and computer science and minoring in data science. I am constantly
+          striving to learn new skills and further develop myself academically
+          and professionally. With course, project, and research experience
+          already under my belt, I am actively searching for an internship in
+          Summer 2024!
         </p>
       </div>
       <Footer />

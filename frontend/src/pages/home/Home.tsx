b112bde7fc10df67f33ca4bfb9b691ad6f47b82a
victor <victor.verma@icloud.com>
2023-11-20 11:11:23 -0500

    created HomeTitle component

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,81 +1,15 @@
 import React from "react";
-import { useEffect, useState } from "react";
 
 import Footer from "../../components/Footer/Footer";
+import HomeTitle from "../../components/HomeTitle/HomeTitle";
 
 import "./Home.css";
 
 const Home = () => {
-  const titlePhrases = [
-    "H",
-    "Hi",
-    "Hi,",
-    "Hi, I",
-    "Hi, I'",
-    "Hi, I'm",
-    "Hi, I'm V",
-    "Hi, I'm Vi",
-    "Hi, I'm Vic",
-    "Hi, I'm Vict",
-    "Hi, I'm Victo",
-    "Hi, I'm Victor",
-    "Hi, I'm Victor",
-    "Hi, I'm Victor",
-    "Hi, I'm Victor",
-    "Hi, I'm Victor",
-    "Hi, I'm Victor",
-    "S",
-    "So",
-    "Sof",
-    "Soft",
-    "Softw",
-    "Softwa",
-    "Softwar",
-    "Software",
-    "Software E",
-    "Software En",
-    "Software Eng",
-    "Software Engi",
-    "Software Engin",
-    "Software Engine",
-    "Software Enginee",
-    "Software Engineer",
-    "Software Engineer",
-    "Software Engineer",
-    "Software Engineer",
-    "Software Engineer",
-    "Software Engineer",
-    "A",
-    "Ac",
-    "Act",
-    "Acti",
-    "Activ",
-    "Active",
-    "Active L",
-    "Active Le",
-    "Active Lea",
-    "Active Lear",
-    "Active Learn",
-    "Active Learne",
-    "Active Learner",
-  ];
-  const [index, setIndex] = useState(0);
-  const [currentTitle, setCurrentTitle] = useState(titlePhrases[index]);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % titlePhrases.length);
-    }, 200);
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    setCurrentTitle(titlePhrases[index] + " ");
-  }, [index]);
-
   return (
     <>
       <div className="home-content">
-        <h1 className="home-title">{currentTitle}</h1>
+        <HomeTitle />
         <p className="home-lead">
           I'm a third-year student at Boston University majoring in mathematics
           and computer science and minoring in data science. I am constantly

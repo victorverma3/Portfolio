83a4b13df8c613849f29745a5e65d86a3af0a0df
victor <victor.verma@icloud.com>
2023-11-20 19:24:00 -0500

    added backend skills support

---
+++ backend/routes/skillRoute.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import { Skills } from "../models/skillModel.js";
+
+const router = express.Router();
+
+// get all skills
+router.get("/", async (request, response) => {
+  try {
+    const skillElements = await Skills.find({}).sort({
+      sortOrder: 1,
+    });
+    return response.status(200).json({
+      count: skillElements.length,
+      data: skillElements,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});

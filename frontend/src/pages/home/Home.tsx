7326674e8535d03c9af0eea26ab31fee223c7389
victor <victor.verma@icloud.com>
2023-11-21 10:16:10 -0500

    fixed css sizing and home lead length

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -14,9 +14,8 @@ const Home = () => {
           I'm a third-year student at Boston University majoring in mathematics
           and computer science and minoring in data science. I am constantly
           striving to learn new skills and further develop myself academically
-          and professionally. With experience in academics, project development,
-          and research already under my belt, I am actively searching for an
-          internship in Summer 2024!
+          and professionally. With experience in both project development and
+          research, I am actively searching for an internship in Summer 2024!
         </p>
       </div>
       <Footer />

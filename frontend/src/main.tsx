be6f7cfdb9f9eafca398fb04f758ab3a756c70ba
victor <victor.verma@icloud.com>
2023-12-13 13:56:52 -0500

    removed index.css import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
-import "./index.css";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router>

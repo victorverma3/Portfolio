cc95c763922275edfc54a1d64cfa82a4ac343936
victor <victor.verma@icloud.com>
2024-01-08 18:46:00 -0500

    updated about me

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -56,25 +56,6 @@ const About = () => {
                 ></img>
             </div>
             <p className="about-bio">
-                I was born and raised in central Massachusetts, and live with my
-                parents, my younger sister, and our dog. I played football for
-                10 years and started at left-guard at the varsity level for my
-                high school team. I'm extremely proud of receiving multiple
-                individual awards in middle school and high school, including
-                being named a Central Mass All Star in 7th grade, but I will
-                forever cherish the close friends and memories I made and the
-                life lessons that I learned while playing the sport.
-            </p>
-            <p className="about-bio">
-                My parents love to travel, and as a result, I have had the lucky
-                opportunity to go to many places growing up. Some of my favorite
-                locations include Egypt for its history, and Iceland and Alaska
-                for their scenery. I have so many more places I want to go,
-                especially the African savanna and the Amazon rainforest. I like
-                nature and the outdoors, and I also want to go camping in the
-                wilderness.
-            </p>
-            <p className="about-bio">
                 I consider myself to be a perpetual learner. Even though a lot
                 of my time is used up with school, working, the gym, and other
                 hobbies, I always spend a few hours each week working on my own
@@ -82,9 +63,32 @@ const About = () => {
                 videos on topics ranging from computer science, mathematics, and
                 physics, to the social sciences and humanities as well. I
                 especially love to immerse myself in the fields of international
-                relations, geopolitical science, and history. I tell everyone
-                that I wish I could experience college as an infinite loop and
-                pick a new major in every pass.
+                relations, geopolitics, and history (and their intersection). I
+                tell everyone that I wish I could experience college as an
+                infinite loop and pick a new major in every pass.
+            </p>
+
+            <p className="about-bio">
+                My parents love to travel, and as a result, I have had the lucky
+                opportunity to go to many places growing up. Some of my favorite
+                locations include Egypt for its history, and Iceland and Alaska
+                for their scenery. I have so many more places I want to go,
+                especially the African savanna and the Amazon rainforest, so I
+                plan to continue exploring the world as I grow older. I like
+                nature and the outdoors, and I also want to go camping in the
+                wilderness.
+            </p>
+
+            <p className="about-bio">
+                I was born and raised in central Massachusetts with my parents,
+                my younger sister, and our dog, Rusty. I played football for 10
+                years and started at left-guard at the varsity level for my high
+                school team. I'm extremely proud of my individual accolades,
+                including being voted lineman of the week by my varsity coaches
+                in my first start and being named a Central Mass All Star in 7th
+                grade, but I will forever cherish the close friends and memories
+                I made and the life lessons that I learned while playing the
+                sport.
             </p>
 
             <br></br>

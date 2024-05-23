fd25a914722d8d66c21b646c085346f8833a5a4a
victor <vpverm@bu.edu>
2024-05-22 20:57:43 -0400

    various updates

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -9,13 +9,14 @@ const Home = () => {
             <div className="w-screen min-h-[80vh] mt-20 mb-[-125px] sm:mt-[110px] sm:mb-[-50px]">
                 <HomeTitle />
                 <p className="w-[65vw] m-auto pt-6 text-xl text-justify sm:w-[60vw] sm:text-2xl sm:mb-12 2xl:text-3xl">
-                    I'm a rising senior at Boston University majoring in
-                    mathematics and computer science and minoring in data
-                    science. I am constantly striving to learn new skills and
-                    further develop myself academically and professionally. I
-                    particularly enjoy both project development and research,
-                    and I'm a software development intern at Savvas Learning
-                    Company in 2024.
+                    I'm a rising senior at Boston University graduating in 2025,
+                    majoring in mathematics and computer science and minoring
+                    data science. I am constantly striving to learn new skills
+                    and continue to develop myself academically and
+                    professionally. I particularly enjoy spending time on
+                    personal projects and research and I will be working as a
+                    software development engineer intern at Savvas Learning
+                    Company in summer 2024.
                 </p>
             </div>
             <Footer />

95865322f37df63abac2e02c85a799a184d302f0
victor <vpverm@bu.edu>
2025-05-20 13:17:11 -0400

    tweaked home text

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -12,8 +12,8 @@ const Home = () => {
                 <p className="w-[65vw] m-auto pt-6 text-xl text-justify sm:w-[60vw] sm:text-2xl sm:mb-12 2xl:text-3xl">
                     I recently graduated from Boston University with a B.A. in
                     Mathematics and Computer Science and a minor in Data
-                    Science. I love to work on projects that apply to my
-                    personal interests.
+                    Science. I love to work on creative projects that solve
+                    problems.
                 </p>
                 <Featured />
             </div>

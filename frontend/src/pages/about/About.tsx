613fa9776ea2fe8a3b19d8c18ed578ed371929d8
Victor Verma <victor.verma@hotmail.com>
2025-10-12 01:28:11 -0400

    updated about

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -39,21 +39,28 @@ const About = () => {
             <h1 className="mt-2 text-5xl 2xl:text-6xl">About Me</h1>
 
             <p className="w-9/12 m-auto pt-6 text-xl 2xl:text-2xl text-justify">
-                I graduated from Boston University in May 2025 with a B.A. in
-                Mathematics and Computer Science and a minor in Data Science.
-                I'm currently working as an associate software engineer at
-                Savvas Learning Company, but I may pursue a graduate degree in
-                computer science. I love learning and spend a lot of time
-                working on personal projects and developing new skills.
+                I'm currently working as an associate software developer in the
+                AI-Enabled Learning domain at Savvas Learning Company, having
+                recently graduated from Boston University with a B.A. in
+                Mathematics and Computer Science and a minor in Data Science. I
+                love learning and solving problems, and my primary interests are
+                machine learning, optimization, and full-stack web development.
             </p>
 
             <p className="w-9/12 m-auto pt-6 text-xl 2xl:text-2xl text-justify">
-                In my free time, I love to watch movies, go to the gym, and
-                solve the Rubik's Cube. I also like to hang out with my friends,
-                watch sports (football, soccer, basketball, and rugby), and eat
-                food of all cuisines. I enjoy traveling, with some of my
-                favorite places being Egypt and Jordan for their history, and
-                Iceland and Alaska for their nature.
+                I pursue many hobbies in my free time, ranging from casual to
+                physical to intellectual. As an avid Letterboxd user and AMC
+                A-List member, I typically watch over 100 movies each year. I
+                live an active lifestyle; I used to play football in high school
+                and rugby in college, both of which I like to watch in addition
+                to soccer, UFC, and basketball. Nowadays, I spend many of my
+                evenings at the Gym. Additionally, I love traveling, and some of
+                my favorite places I've visited are Egypt and Jordan for their
+                history, and Iceland and Alaska for their nature. My bucket list
+                currently includes gorilla trekking in Rwanda and hiking Machu
+                Picchu in Peru. Lastly, I've starting reading more since
+                graduating college, and my favorite genres are mystery, history,
+                international geopolitics, and sci-fi.
             </p>
 
             <br />

9cc88089f4246cbed1030a5ca327befe8766cd9d
Victor Verma <victor.verma@hotmail.com>
2025-10-18 13:58:07 -0400

    updated home text

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -10,10 +10,9 @@ const Home = () => {
             <div className="w-screen sm:min-h-[65vh] pt-20 sm:pt-28">
                 <HomeTitle />
                 <p className="w-3/5 max-w-3xl m-auto pt-6 text-xl text-justify sm:w-[60vw] sm:text-2xl sm:mb-12 2xl:text-3xl">
-                    I recently graduated from Boston University with a B.A. in
-                    Mathematics and Computer Science and a minor in Data
-                    Science. I love to work on creative projects that solve
-                    problems.
+                    I'm an associate software developer in the AI Enabled
+                    Learning domain at Savvas Learning Company. I love to work
+                    on creative projects that solve problems.
                 </p>
                 <Featured />
             </div>
